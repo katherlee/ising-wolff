5198e5ded8935e4b983cce3b1e6fda8c4947551b
katherlee <lijia1608@gmail.com>
2014-04-14 22:56:41 +0800

    test file for MKL_VSL RNGs

---
+++ rng_demo.f90
@@ -0,0 +1,42 @@
+include 'mkl_vsl.f90'
+
+program MKL_VSL_TEST
+
+use MKL_VSL_TYPE
+use MKL_VSL
+
+REAL(kind = 8) r(100000)
+real(kind = 8) s
+real(kind = 8) a, sigma
+
+TYPE (VSL_STREAM_STATE) :: stream
+
+integer*4 errcode
+integer*4 i,j
+integer brng, method, seed, n
+
+n = 100000
+s = 0.0
+a = 0.0
+sigma = 1.0
+method = VSL_BRNG_METHOD_UNIFORM_STD
+brng = VSL_BRNG_MT2203
+seed = 777
+
+errcode = vslnewstream( stream, brng, seed )
+
+open(1,file="test.dat")
+do i=1, 10
+   errcode = vdrnguniform( method, stream, n, r, a, sigma )
+   do j = 2, n
+      s = s+r(j)
+      write (1,*) r(j), r(j-1)
+   end do
+end do
+close(1)
+
+errcode = vsldeletestream( stream)
+
+print *, "mean of normal distribution", s
+
+end program MKL_VSL_TEST
