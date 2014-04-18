ca3ecb2a8d0c3d59654355ca475370ca3ce98920
katherlee <lijia1608@gmail.com>
2014-04-18 22:00:39 +0800

    First successful m curve with l=100

--- rng_demo.f90
+++ rng_demo.f90
@@ -5,8 +5,7 @@ program MKL_VSL_TEST
 use MKL_VSL_TYPE
 use MKL_VSL
 
-REAL(kind = 8) r(100000)
-real(kind = 8) s
+REAL(kind = 8), dimension(10000):: r
 real(kind = 8) a, b
 
 TYPE (VSL_STREAM_STATE) :: stream
@@ -19,24 +18,19 @@ n = 100000
 s = 0.0
 a = 0.0
 b = 1.0
-method = VSL_BRNG_METHOD_UNIFORM_STD
+method = VSL_RNG_METHOD_EXPONENTIAL_ICDF
 brng = VSL_BRNG_MT2203
 seed = 777
 
 errcode = vslnewstream( stream, brng, seed )
 
-open(1,file="test.dat")
-do i=1, 10
-   errcode = vdrnguniform( method, stream, n, r, a, b )
-   do j = 2, n
-      s = s+r(j)
-      write (1,*) r(j), r(j-1)
-   end do
+errcode = vdrngexponential( method, stream, 10000, r, a, b )
+
+open(1,file='test.txt')
+do i=1,10000
+write (1,*) r(i)
 end do
 close(1)
-
 errcode = vsldeletestream( stream)
-
-print *, "mean of normal distribution", s
 
 end program MKL_VSL_TEST
